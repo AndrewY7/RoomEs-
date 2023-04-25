9824c1c7e278f5f69709ab1e0dc5e9ad8d6b0e09
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2023-04-24 23:49:43 -0400

    Initial Commit

---
+++ RoomEs/RoomEsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomEsApp.swift
+//  RoomEs
+//
+//  Created by Andrew Yang on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomEsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
